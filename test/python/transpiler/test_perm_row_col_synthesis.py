d75546c65073bef9cac21142097cc1232c8f74ff
susanna <susannae.repo@gmail.com>
2022-11-22 19:17:40 +0200

    Edit input dag for tests, extract returned circuit
    - The circuit extraction is not working correctly, two cnots have switched places

--- test/python/transpiler/test_perm_row_col_synthesis.py
+++ test/python/transpiler/test_perm_row_col_synthesis.py
@@ -6,17 +6,19 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 import numpy as np
+from qiskit.circuit.quantumregister import QuantumRegister
 
 from qiskit.test import QiskitTestCase
 from qiskit.transpiler.passes.synthesis.perm_row_col_synthesis import PermRowColSynthesis
 from qiskit.transpiler.passes.synthesis.linear_functions_synthesis import LinearFunctionsSynthesis
 from qiskit import QuantumCircuit
-from qiskit.converters import circuit_to_dag, isinstanceint
+from qiskit.converters import circuit_to_dag, dag_to_circuit
 from qiskit.dagcircuit.dagcircuit import DAGCircuit
 from qiskit.transpiler import CouplingMap, coupling
 from qiskit.circuit.library.generalized_gates.linear_function import LinearFunction
 from qiskit.circuit.library.generalized_gates.permutation import Permutation
 from qiskit.transpiler.passes.optimization.collect_linear_functions import CollectLinearFunctions
+
 
 class TestPermRowColSynthesis(QiskitTestCase):
     """Test PermRowColSynthesis"""
@@ -28,7 +30,7 @@ class TestPermRowColSynthesis(QiskitTestCase):
                 qargs = range(len(circuit.qubits))
             else:
                 qargs = range(len(circuit))
-            
+
             dag.apply_operation_back(LinearFunction(circuit), qargs)
 
         return dag
@@ -53,10 +55,17 @@ class TestPermRowColSynthesis(QiskitTestCase):
     def test_run(self, mock_perm_row_col):
         """Test run method"""
         empty = QuantumCircuit(6)
+        empty.cx(0, 1)
+        empty.cx(1, 5)
+        empty.cx(3, 1)
+        empty.cx(1, 4)
+        empty.cx(1, 3)
+        empty.cx(3, 5)
+        empty.cx(2, 1)
         collec = CollectLinearFunctions()
         e_dag = circuit_to_dag(empty)
         dag = collec.run(e_dag)
-        
+
         coupling = CouplingMap()
         circ = QuantumCircuit(6)
         circ.cx(1, 0)
@@ -74,17 +83,23 @@ class TestPermRowColSynthesis(QiskitTestCase):
         circ.cx(5, 2)
 
         perm = Permutation(6, [5, 3, 1, 0, 4, 2])
-        
+
         mock_perm_row_col.return_value = (circ, perm)
-        
-        
+
         synthesis = PermRowColSynthesis(coupling)
 
         instance = synthesis.run(dag)
-        
 
+        ret_circ = dag_to_circuit(instance)
 
-        
+        cnots = []
+        for d in ret_circ.data:
+            if d.operation.name == "cx":
+                cnots.append(d)
+
+        ret_cnots = QuantumCircuit.from_instructions(cnots)
+
+        self.assertTrue(mock_perm_row_col.called)
 
 
 if __name__ == "__main__":
