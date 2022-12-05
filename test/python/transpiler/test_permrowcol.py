a0ee38e739e576e98786aec49ea09708f607d3f3
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2022-12-05 10:22:21 +0200

    remove wrong test

--- test/python/transpiler/test_permrowcol.py
+++ test/python/transpiler/test_permrowcol.py
@@ -10,6 +10,8 @@ from qiskit.circuit.library import LinearFunction
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit.transpiler import CouplingMap
 from qiskit.circuit.library.generalized_gates.permutation import Permutation
+from qiskit.transpiler.synthesis.matrix_utils import build_random_parity_matrix
+from qiskit.providers.fake_provider import FakeManilaV2
 
 
 class TestPermRowCol(QiskitTestCase):
@@ -526,6 +528,21 @@ class TestPermRowCol(QiskitTestCase):
         print(instance[0])
         print(instance[1])
         self.assertEqual(np.array_equal(parity_mat, correct_permutation_matrix), True)
+
+
+    # def test_perm_row_col_returns_valid_output_with_a_common_case(self):
+    #     """Test the output of perm_row_col for correctness"""
+    #     backend = FakeManilaV2()
+    #     coupling_map = backend.coupling_map
+    #     coupling = CouplingMap(coupling_map)
+    #     permrowcol = PermRowCol(coupling)
+    #     parity_mat = build_random_parity_matrix(42, 5, 60)
+    #     original_parity_map = parity_mat.copy()
+    #     circuit, perm = permrowcol.perm_row_col(parity_mat)
+    #     circuit_matrix = LinearFunction(circuit).linear.astype(int)
+
+    #     instance = np.matmul(circuit_matrix, parity_mat)
+    #     self.assertEqual(np.array_equal(instance, original_parity_map), True)
 
 
 if __name__ == "__main__":
