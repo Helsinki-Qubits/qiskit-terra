68b50dc9b964f6dca4c493b8a24f54eee574bd54
Milla Kelhu <milla.kelhu@gmail.com>
2022-12-08 19:50:47 +0200

    Add old test
    Added the test after it disappeared in a merge conflict

--- test/python/transpiler/test_permrowcol.py
+++ test/python/transpiler/test_permrowcol.py
@@ -6,8 +6,7 @@ import retworkx as rx
 
 from qiskit.test import QiskitTestCase
 from qiskit.transpiler.synthesis.permrowcol import PermRowCol
-from qiskit.circuit.library import LinearFunction
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
 from qiskit.transpiler import CouplingMap
 from qiskit.circuit.library.generalized_gates.permutation import Permutation
 from qiskit.transpiler.synthesis.matrix_utils import build_random_parity_matrix
@@ -713,6 +712,19 @@ class TestPermRowCol(QiskitTestCase):
 
         instance = np.matmul(circuit_matrix, parity_mat)
 
+        self.assertTrue(np.array_equal(instance, original_parity_map))
+
+    def test_common_case_with_complete_graph(self):
+        """Test common_case with complete graph"""
+        n = 6
+        parity_mat = build_random_parity_matrix(42, n, 60)
+        coupling_list = [(i, j) for i in range(n) for j in range(n) if i != j]
+        original_parity_map = parity_mat.copy()
+        coupling = CouplingMap(coupling_list)
+        permrowcol = PermRowCol(coupling)
+        circuit, perm = permrowcol.perm_row_col(parity_mat)
+        circuit_matrix = LinearFunction(circuit).linear
+        instance = np.matmul(circuit_matrix, parity_mat)
         self.assertTrue(np.array_equal(instance, original_parity_map))
 
     def test_add_cnot_adds_corresponding_row_operations_on_parity_matrix(self):
